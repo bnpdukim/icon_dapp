22487e88ba8053f9679246100cc7d45fa29585aa
dukim <sajacall82@naver.com>
2019-06-12 16:30:44 +0900

    game room added

--- week04/dice_game/dice_game.py
+++ week04/dice_game/dice_game.py
@@ -6,6 +6,9 @@ class DiceGame(IconScoreBase):
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
+        self._db = db
+        self._DDB_hand = DictDB(self._HAND, db, value_type=str)
+        self._DDB_ready = DictDB(self._READY, db, value_type=bool)
 
     def on_install(self) -> None:
         super().on_install()
@@ -17,6 +20,10 @@ class DiceGame(IconScoreBase):
         input_data = f'{self.block.timestamp}, {data}'.encode()
         hash = sha3_256(input_data)
         return int.from_bytes(hash, 'big')
+
+    @external(readonly=True)
+    def join(self, _from: Address) -> int:
+
 
     @external(readonly=True)
     def roll_dice(self, _from: Address) -> int:
